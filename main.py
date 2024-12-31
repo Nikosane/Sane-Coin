8d7b1c690ae863f6b3ab128903916f9252e9c986
Nikosane <linux.nikosane@gmail.com>
2024-12-31 22:52:32 +0530

    entry point of Sanecoin

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from text_file_processor import generate_chunks
-from encryption import encrypt_chunks, decrypt_chunks
-from key_generator import generate_random_key
-from word_selector import select_word
 from game_logic import start_game
+from utils import load_words
 
 def main():
-    # Load words and generate chunks
-    words = generate_chunks("words.txt")
-    
-    # Generate a random encryption key
-    encryption_key = generate_random_key()
-    
-    # Encrypt chunks
-    encrypted_chunks = encrypt_chunks(words, encryption_key)
-    
-    # Select a random word as the desired word
-    desired_word = select_word(words)
+    # Load words from word.txt
+    words = load_words("words.txt")
     
     # Start the game logic
-    start_game(encrypted_chunks, desired_word)
+    start_game(words)
 
 if __name__ == "__main__":
     main()
+
