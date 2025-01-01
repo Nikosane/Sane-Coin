bc4c2cdcb024c789eeb56bf4cce60f2228c7170e
Nikosane <linux.nikosane@gmail.com>
2025-01-01 23:14:01 +0530

    entry point of the mine

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from game_logic import start_game
+from game_logic import start_game 
 from utils import load_words
 
 def main():
+
     # Load words from word.txt
     words = load_words("words.txt")
+
     
     # Start the game logic
     start_game(words)
